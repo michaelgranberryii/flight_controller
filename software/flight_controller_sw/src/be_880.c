c90ee9bb60b420142e71a82a3851a69cde5d1209
michaelgranberryii <michaelgranberryii@gmail.com>
2024-02-04 22:19:51 -0800

    added uart loopback for interrupt testing

--- software/flight_controller_sw/src/be_880.c
+++ software/flight_controller_sw/src/be_880.c
@@ -1,5 +1,11 @@
 #include "be_880.h"
 
+u8 SendBuffer[TEST_BUFFER_SIZE];
+u8 ReceiveBuffer[TEST_BUFFER_SIZE];
+
+static volatile int TotalReceivedCount;
+static volatile int TotalSentCount;
+
 XUartLite_Config *uart_config1;
 XUartLite uart1;
 
@@ -19,23 +25,84 @@ void uart_be_init(){
 	}
 }
 
-void read_be_data() {
-	u8 data1[100];
-	s8 data_size = sizeof(data1);
-	u8 Rx[data_size];
+s32 uart_be_intr_init(XScuGic *gic) {
+	int Status;
+	XScuGic_SetPriorityTriggerType(gic, XPAR_FABRIC_UARTLITE_0_VEC_ID, 0xA0, 0x3);
+	Status = XScuGic_Connect(gic, XPAR_FABRIC_UARTLITE_0_VEC_ID,
+				 (Xil_ExceptionHandler) XUartLite_InterruptHandler,
+				 &uart1);
+	if (Status != XST_SUCCESS) {
+		return Status;
+	}
+	XScuGic_Enable(gic, XPAR_FABRIC_UARTLITE_0_VEC_ID);
 
-	int byteRcvd = 0;
 
-	while(byteRcvd != data_size) {
-		byteRcvd = byteRcvd + XUartLite_Recv(&uart1, &Rx[byteRcvd], data_size);
+	XUartLite_SetSendHandler(&uart1, SendHandler, &uart1);
+	XUartLite_SetRecvHandler(&uart1, RecvHandler, &uart1);
+
+	/*
+	 * Enable the interrupt of the UartLite so that interrupts will occur.
+	 */
+	XUartLite_EnableInterrupt(&uart1);
+	return XST_SUCCESS;
+}
+
+void uart_loopback_test() {
+	int Index;
+	/*
+	 * Initialize the send buffer bytes with a pattern to send and the
+	 * the receive buffer bytes to zero to allow the receive data to be
+	 * verified.
+	 */
+	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
+		SendBuffer[Index] = Index;
+		ReceiveBuffer[Index] = 0;
 	}
 
+	/*
+	 * Start receiving data before sending it since there is a loopback.
+	 */
+	XUartLite_Recv(&uart1, ReceiveBuffer, TEST_BUFFER_SIZE);
 
-	xil_printf("GPS data: \n\r");
+	/*
+	 * Send the buffer using the UartLite.
+	 */
+	XUartLite_Send(&uart1, SendBuffer, TEST_BUFFER_SIZE);
 
-	for(int i = 0 ; i < data_size; i++)
-	{
-		xil_printf("%c",Rx[i]);
+	/*
+	 * Wait for the entire buffer to be received, letting the interrupt
+	 * processing work in the background, this function may get locked
+	 * up in this loop if the interrupts are not working correctly.
+	 */
+	while ((TotalReceivedCount != TEST_BUFFER_SIZE)
+	       || (TotalSentCount != TEST_BUFFER_SIZE)) {
 	}
-	xil_printf("\n\r\n\r");
+
+	/*
+	 * Verify the entire receive buffer was successfully received.
+	 */
+//	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
+//		if (ReceiveBuffer[Index] != SendBuffer[Index]) {
+//			return XST_FAILURE;
+//		}
+//	}
+
+}
+
+void test() {
+	XUartLite_Recv(&uart1, ReceiveBuffer, TEST_BUFFER_SIZE);
+}
+
+void SendHandler(void *CallBackRef, unsigned int EventData)
+{
+	TotalSentCount = EventData;
+	xil_printf("Called SendHandler: %d\n\r", TotalSentCount);
+
+}
+
+void RecvHandler(void *CallBackRef, unsigned int EventData)
+{
+	TotalReceivedCount = EventData;
+	xil_printf("Called RecvHandler: %d\n\r", TotalSentCount);
+//	xil_printf("ReceiveBuffer: \n\r %s\n\r", ReceiveBuffer);
 }
--- software/flight_controller_sw/src/be_880.h
+++ software/flight_controller_sw/src/be_880.h
@@ -4,8 +4,15 @@
 #include "xuartlite.h"
 #include "xparameters.h"
 #include "xil_printf.h"
+#include "xil_exception.h"
+#include "xscugic.h"
+
+#define TEST_BUFFER_SIZE        2048
 
 void uart_be_init();
-void read_be_data();
-
+void uart_loopback_test();
+s32 uart_be_intr_init(XScuGic *gic);
+void test();
+void SendHandler(void *CallBackRef, unsigned int EventData);
+void RecvHandler(void *CallBackRef, unsigned int EventData);
 #endif  /* end of protection macro */
