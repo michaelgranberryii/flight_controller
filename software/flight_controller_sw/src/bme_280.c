db18ba5be4b21c9fc266bf1e935e3c477d2cc9fb
michaelgranberryii <michaelgranberryii@gmail.com>
2024-02-06 06:23:21 -0800

    added bme code

--- software/flight_controller_sw/src/bme_280.c
+++ software/flight_controller_sw/src/bme_280.c
@@ -12,15 +12,67 @@
 IIC iic_bme;
 
 void iic_bme_init() {
+	iic_bme.chipAddr = BME_280_ADDRESS;
 	iic_begin(&iic_bme, XPAR_AXI_IIC_0_DEVICE_ID, BME_280_ADDRESS);
-	iic_bme_setup();
+	bme_280_setup();
 }
 
-void iic_bme_setup() {
-
+void bme_280_setup() {
+	bme_280_reset(0xB6);
+	bme_280_read_id();
+	bme_280_ctrl_hum();
+	bme_280_ctrl_meas();
 }
 
-void read_bme() {
-    uint8_t bme_280_hum_lsb;
+void bme_280_reset(u8 reset) {
+	write_iic(&iic_bme, BME_280_RESET, &reset, 1);
+}
+
+void bme_280_read_id() {
+	uint8_t bme_280_id = 0;
+	read_iic(&iic_bme, BME_280_ID, &bme_280_id, 1);
+	xil_printf("bme_280_id: %x\n\r", bme_280_id);
+}
+
+void bme_280_ctrl_hum() {
+	u8 options = BME_280_OSRS_H1;
+	write_iic(&iic_bme, BME_280_CTRL_HUM, &options, 1);
+}
+
+void bme_280_ctrl_meas() {
+	u8 options = BME_280_OSRS_T1 | BME_280_OSRS_P1 | BME_280_NORMAL_MODE;
+	write_iic(&iic_bme, BME_280_CTRL_MEAS, &options, 1);
+}
+
+void bme_280_read_hum() {
+    uint8_t bme_280_hum_lsb = 0;
+    uint8_t bme_280_hum_msb = 0;
     read_iic(&iic_bme, BME_280_HUM_LSB, &bme_280_hum_lsb, 1);
+    read_iic(&iic_bme, BME_280_HUM_MSB, &bme_280_hum_msb, 1);
+    xil_printf("bme_280_hum_lsb: %x\n\r", bme_280_hum_lsb);
+    xil_printf("bme_280_hum_msb: %x\n\r", bme_280_hum_msb);
+}
+
+void bme_280_read_temp() {
+    uint8_t bme_280_temp_xlsb = 0;
+    uint8_t bme_280_temp_lsb = 0;
+    uint8_t bme_280_temp_msb = 0;
+    read_iic(&iic_bme, BME_280_TEMP_XLSB, &bme_280_temp_xlsb, 1);
+    read_iic(&iic_bme, BME_280_TEMP_LSB, &bme_280_temp_lsb, 1);
+    read_iic(&iic_bme, BME_280_TEMP_MSB, &bme_280_temp_msb, 1);
+    xil_printf("bme_280_temp_xlsb: %x\n\r", bme_280_temp_xlsb);
+    xil_printf("bme_280_temp_lsb: %x\n\r", bme_280_temp_lsb);
+    xil_printf("bme_280_temp_msb: %x\n\r", bme_280_temp_msb);
+}
+
+void bme_280_read_press() {
+    uint8_t bme_280_press_xlsb = 0;
+    uint8_t bme_280_press_lsb = 0;
+    uint8_t bme_280_press_msb = 0;
+    read_iic(&iic_bme, BME_280_PRESS_XLSB, &bme_280_press_xlsb, 1);
+    read_iic(&iic_bme, BME_280_PRESS_LSB, &bme_280_press_lsb, 1);
+    read_iic(&iic_bme, BME_280_PRESS_MSB, &bme_280_press_msb, 1);
+    xil_printf("bme_280_press_xlsb: %x\n\r", bme_280_press_xlsb);
+    xil_printf("bme_280_press_lsb: %x\n\r", bme_280_press_lsb);
+    xil_printf("bme_280_press_msb: %x\n\r", bme_280_press_msb);
 }
--- software/flight_controller_sw/src/bme_280.h
+++ software/flight_controller_sw/src/bme_280.h
@@ -4,13 +4,60 @@
 #include "xiic.h"
 
 // Default I2C address for the BME_280
-#define BME_280_ADDRESS                        	0x68
+#define BME_280_ADDRESS                        	0x76
 
 #define BME_280_HUM_LSB                   		0xFE
 #define BME_280_HUM_MSB                   		0xFD
 
+#define BME_280_TEMP_XLSB						0xFC
+#define BME_280_TEMP_LSB						0xFB
+#define BME_280_TEMP_MSB						0xFA
+
+#define BME_280_PRESS_XLSB						0xF9
+#define BME_280_PRESS_LSB						0xF8
+#define BME_280_PRESS_MSB						0xF7
+
+#define BME_280_CONFIG							0xF5
+
+#define BME_280_CTRL_MEAS						0xF4
+#define BME_280_SLEEP_MODE						0x0
+#define BME_280_FORCED_MODE_1					0x1
+#define BME_280_FORCED_MODE_2					0x2
+#define BME_280_NORMAL_MODE						0x3
+#define BME_280_OSRS_P0							(0x0 << 2)
+#define BME_280_OSRS_P1							(0x1 << 2)
+#define BME_280_OSRS_P2							(0x2 << 2)
+#define BME_280_OSRS_P4							(0x3 << 2)
+#define BME_280_OSRS_P8							(0x4 << 2)
+#define BME_280_OSRS_P16						(0x5 << 2)
+#define BME_280_OSRS_T0							(0x0 << 5)
+#define BME_280_OSRS_T1							(0x1 << 5)
+#define BME_280_OSRS_T2							(0x2 << 5)
+#define BME_280_OSRS_T4							(0x3 << 5)
+#define BME_280_OSRS_T8							(0x4 << 5)
+#define BME_280_OSRS_T16						(0x5 << 5)
+
+#define BME_280_STATUS							0xF3
+
+#define BME_280_CTRL_HUM						0xF2
+#define BME_280_OSRS_H0							0x0
+#define BME_280_OSRS_H1							0x1
+#define BME_280_OSRS_H2							0x2
+#define BME_280_OSRS_H4							0x3
+#define BME_280_OSRS_H8							0x4
+#define BME_280_OSRS_H16						0x5
+
+#define BME_280_RESET							0xE0
+#define BME_280_ID								0xD0
+
 void iic_bme_init();
-void iic_bme_setup();
-void read_bme();
+void bme_280_setup();
+void bme_280_reset(u8 reset);
+void bme_280_read_id();
+void bme_280_ctrl_hum();
+void bme_280_ctrl_meas();
+void bme_280_read_hum();
+void bme_280_read_temp();
+void bme_280_read_press();
 
 #endif  /* end of protection macro */
